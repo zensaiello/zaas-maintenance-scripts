af445276314b06ef5983c0bcdef68e97e7b0abbb
root <root@sma-dev04.zenoss.lab>
2018-09-07 10:40:49 -0500

    initial pass on script conversion

--- maintenance_scripts/fstrim.sh
+++ maintenance_scripts/fstrim.sh
@@ -4,23 +4,22 @@
 # fstrim
 #
 
-# Add zenoss_api function to script
-cd /opt/zaas/maintenance/scripts
-
-if [[ -f zenoss_json.sh ]]; then
-    source ./zenoss_json.sh
-else
-    echo "Unable to find zenoss_json.sh. Script will exit."
+if [ -z "${topSD}" ] ; then 
+    sd=$(echo "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )")
+    source ${sd}/../setScriptEnv.sh
+fi 
+if [ ! -f "${apiSD}/zenApiCli.py" ]; then
+    echo "Unable to find '${apiSD}/zenApiCli.py'. Script will exit."
     exit
 fi
 
 IPADDR=$(hostname -i)
 TARGET=$(hostname -s)
-DEVICE=$(getZenossDeviceByIP "$IPADDR")
+DEVICE=$("${apiSD}/zenApiCli.py" -r DeviceRouter -m getDevices -d keys="['name','ipAddress']",params="{'ipAddress': '${IPADDR}'}" -x result.devices.0.name | cut -d":" -f2)
 COMPONENT="FSTrim"
 
-UUID=$(writeZenossInfoEvent "$DEVICE" "$COMPONENT" "$COMPONENT job has started for $TARGET.")
+UUID=$("${apiSD}/zenApiCli.py" -r EventsRouter -m add_event -d summary="${COMPONENT}",component="${COMPONENT}",device="${DEVICE}",severity=3,evclass="/Cmd",evclasskey=""  -x uuid | cut -d":" -f2)
 OUTPUT=$(/usr/sbin/fstrim -av)
 
 sleep 5
-clearZenossEvent "$DEVICE" "$COMPONENT" "$COMPONENT job has completed for $TARGET. Results: $OUTPUT" "$UUID"
+CLEAR=$("${apiSD}/zenApiCli.py" -r EventsRouter -m add_event -d summary="${COMPONENT} job has completed for ${TARGET}. Results: ${OUTPUT}",component="${COMPONENT}",device="${DEVICE}",severity=0,evclass="/Cmd",evclasskey="${UUID}")
---
+++ setScriptEnv.sh
@@ -0,0 +1,5 @@
+
+topSD=`echo "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"`
+zaasSD="${topSD}/maintenance_scripts"
+svcsSD="${topSD}/services"
+apiSD="${topSD}/zenoss-RM-api"
